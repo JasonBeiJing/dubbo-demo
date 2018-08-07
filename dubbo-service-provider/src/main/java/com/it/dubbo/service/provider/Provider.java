523369313b2f5882ff1ee30e684a9bb98768bad1
wangshujie <colday1103@126.com>
2018-08-07 19:55:23 +0800

    more configuration supported with dubbo

--- dubbo-service-provider/src/main/java/com/it/dubbo/service/provider/Provider.java
+++ dubbo-service-provider/src/main/java/com/it/dubbo/service/provider/Provider.java
@@ -3,6 +3,7 @@ package com.it.dubbo.service.provider;
 import java.io.IOException;
 
 import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.ProtocolConfig;
 import com.alibaba.dubbo.config.RegistryConfig;
 import com.alibaba.dubbo.config.ServiceConfig;
 import com.it.dubbo.service.api.HelloService;
@@ -12,10 +13,26 @@ public class Provider {
 	public static void main(String[] args) throws IOException {
 		ServiceConfig<HelloService> serviceConfig = new ServiceConfig<HelloService>();
         serviceConfig.setApplication(new ApplicationConfig("dubbo-service-provider"));
+        
+        //RegistryConfig rc = new RegistryConfig("zookeeper://127.0.0.1:2181");
+        //rc.setTimeout(timeout);
+        //rc.setGroup(group);
+        //rc.setProtocol(protocol);
+        //rc.setRegister(register);
+        //rc set ....
         serviceConfig.setRegistry(new RegistryConfig("zookeeper://127.0.0.1:2181"));
+        
+        //ProtocolConfig pc = new ProtocolConfig();
+        //serviceConfig.setProtocol(pc);
+        
+        //ServiceConfig<HelloService>  sc = new ServiceConfig<>();
+        //sc.setRef(new HelloServiceImpl());
+        //sc.setInterface(HelloService.class);
         serviceConfig.setInterface(HelloService.class);
         serviceConfig.setRef(new HelloServiceImpl());
+        
         serviceConfig.export();
+        
         System.in.read();
 	}
 }
